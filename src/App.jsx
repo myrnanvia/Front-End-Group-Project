24b7eeed702b260c610f1589372172130f3da755
myrnanvia <myrnanovia811@gmail.com>
2021-11-19 20:17:28 +0700

    nfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/welcome" element={<Home/>}/>
+        <Route path="/products/:category" element={<ProductList/>} />
+        <Route path="/products/:id" element={<Product/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,68 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/tpqGxw0/2.png" ,
+        title: "FLASH SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/2ZV87Pz/3.png" ,
+        title: "BIG DISCOUNT",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/xjZS27R/4.png" ,
+        title: "11.11",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "fbf0f4",
+    }
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/6Z18zHV/smiling-beautiful-young-woman-pink-mini-dress-posing-studio-155003-14602.jpg",
+        title: "DRESS",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/r43GHsc/young-beautiful-woman-looking-camera-trendy-girl-casual-summer-white-t-shirt-jeans-shorts-round-sung.jpg",
+        title: "SHIRT",
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/mSSGpJL/excited-ginger-lady-hat-holding-straw-bag-ecstatic-long-haired-girl-summer-outfit-enjoying-good-day.jpg",
+        title: "SKIRT",
+    }
+]
+
+export const popularProducts = [
+    {
+        id: 1,
+        img:"https://i.ibb.co/HFtBnTw/trendy-young-girl-outside-23-2147778697.jpg",
+    },
+    {
+        id: 2,
+        img:"https://i.ibb.co/2ydknzF/fashion-portrait-young-elegant-woman-1328-2743.jpg",
+    },
+    {
+        id: 3,
+        img:"https://i.ibb.co/Kxgtnrq/outdoor-summer-portrait-young-stylish-girl-posed-sunny-day-street-1328-4419.jpg",
+    },
+    {
+        id: 4,
+        img:"https://i.ibb.co/n63gQQY/fashionable-pale-brunette-long-green-dress-black-jacket-sunglasses-standing-street-during-daytime-ag.jpg",
+    },
+    {
+        id: 5,
+        img:"https://i.ibb.co/2q6w2wf/barefoot-legs-female-group-jeans-23-2148206850.jpg",
+    },
+    {
+        id: 6,
+        img:"https://i.ibb.co/HCgKh8r/woman-white-basic-sweater-with-design-space-casual-apparel-full-body-set-53876-125245.jpg",
+    },
+]
